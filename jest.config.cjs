11c51cead562d248d8ed5d8a1d169e513e72d4c6
gaurav <gaurav.soni@thriwe.com>
2025-10-31 18:00:19 +0530

    testing environment setup

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+};
